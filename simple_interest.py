cf1b69cc6eed4564559f92a9c246813db35ee2a3
happybansal-0802 <77833350+happybansal-0802@users.noreply.github.com>
2021-03-19 20:31:56 +0530

    simple_interest.py

---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+'''simple interest'''
+p=eval(input("enter the principal amount"))
+r=eval(input("enter the rate"))
+t=eval(input("enter the time"))
+s=(p*r*t)/100
+a=p+s
+print("simple interest is",s)
+print("total amount is ",a)
